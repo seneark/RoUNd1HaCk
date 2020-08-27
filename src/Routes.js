f396dc3502d4b09e847c8b8a17dbd171fc419648
seneark <seneark@protonmail.com>
2020-08-27 15:14:47 +0530

    Complete for first screening

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Switch,Route } from 'react-router-dom';
 
 import Round1 from "./Views/Round1";
-import Home from "./Views/home";
 const Routes = (props) => {
     return (
         <Switch>
-            <Route path={process.env.PUBLIC_URL + "/"} component={Home}/>
-            <Route path={process.env.PUBLIC_URL + "/Round1"} component={Round1}/>
+            <Route path={process.env.PUBLIC_URL + "/"} exact component={Round1}/>
         </Switch>
     );
 };
